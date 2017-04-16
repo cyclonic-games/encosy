bb95c8622464cfa47138ff50320d56c045d61f16
Nick Dugger <email@nickdugger.com>
2017-04-16 00:53:48 -0500

    Fixed invalid webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'encosy.js'),
+    entry: path.join(__dirname, 'src', 'index.js'),
 
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'engine.js'
+        filename: 'encosy.js'
     },
 
     module: {
