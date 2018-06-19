a246b81fee541d3d4621081738fe6c7331cfbd4e
Nick Dugger <email@nickdugger.com>
2018-06-18 20:45:03 -0500

    module.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.Component = require('./lib/component');
-exports.Entity = require('./lib/entity');
-exports.System = require('./lib/system');
+module.exports.Component = require('./lib/component');
+module.exports.Entity = require('./lib/entity');
+module.exports.System = require('./lib/system');
